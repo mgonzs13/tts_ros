07fdd7e07531c76b9d5d42fa49f574ef4fd5f2d6
Matt Williamson <matt@aimatt.com>
2024-05-09 18:28:45 -0400

    Added streaming so the time to first speech is fast

--- tts_ros/tts_ros/tts_node.py
+++ tts_ros/tts_ros/tts_node.py
@@ -30,6 +30,7 @@ import pyaudio
 import tempfile
 import threading
 from TTS.api import TTS as TtsModel
+import numpy as np
 
 import rclpy
 from rclpy.node import Node
@@ -56,7 +57,8 @@ class AudioCapturerNode(Node):
             ("model", "tts_models/en/ljspeech/vits"),
             ("speaker_wav", ""),
             ("speaker", ""),
-            ("device", "cpu")
+            ("device", "cpu"),
+            ("stream", False),
         ])
 
         self.chunk = self.get_parameter(
@@ -72,6 +74,8 @@ class AudioCapturerNode(Node):
             "speaker").get_parameter_value().string_value
         self.device = self.get_parameter(
             "device").get_parameter_value().string_value
+        self.stream = self.get_parameter(
+            "stream").get_parameter_value().bool_value
 
         if (
             not self.speaker_wav or
@@ -104,6 +108,15 @@ class AudioCapturerNode(Node):
             callback_group=ReentrantCallbackGroup()
         )
 
+        self.get_logger().info(f"Stream: {self.stream} {self.speaker_wav}")
+
+        if self.stream and self.speaker_wav:
+            self.get_logger().info(f"Streaming. Getting embeddings from {self.speaker_wav}")
+            self.gpt_cond_latent, self.speaker_embedding = self.tts.synthesizer.tts_model.get_conditioning_latents(audio_path=[self.speaker_wav])
+        else:
+            self.stream = False
+
+
         self.get_logger().info("TTS node started")
 
     def destroy_node(self) -> bool:
@@ -127,60 +140,132 @@ class AudioCapturerNode(Node):
 
         request: TTS.Goal = goal_handle.request
 
+        start_time = time.time()
+        
         text = request.text
         language = request.language
 
         if not self.tts.is_multi_lingual:
             language = None
 
-        # create audio file
-        audio_file = tempfile.NamedTemporaryFile(mode="w+")
-        self.tts.tts_to_file(
-            text,
-            speaker_wav=self.speaker_wav,
-            speaker=self.speaker,
-            language=language,
-            file_path=audio_file.name
-        )
+        if not self.stream:
+            # create audio file
+            audio_file = tempfile.NamedTemporaryFile(mode="w+")
+            self.tts.tts_to_file(
+                text,
+                speaker_wav=self.speaker_wav,
+                speaker=self.speaker,
+                language=language,
+                file_path=audio_file.name
+            )
 
-        # pub audio
-        audio_file.seek(0)
-        wf = wave.open(audio_file.name, "rb")
-        audio_file.close()
-        audio_format = pyaudio.get_format_from_width(wf.getsampwidth())
+            # pub audio
+            audio_file.seek(0)
+            wf = wave.open(audio_file.name, "rb")
+            audio_file.close()
+            audio_format = pyaudio.get_format_from_width(wf.getsampwidth())
 
-        frequency = wf.getframerate() / self.chunk
-        pub_rate = self.create_rate(frequency)
+            frequency = wf.getframerate() / self.chunk
+            pub_rate = self.create_rate(frequency)
 
-        # send audio data
-        data = wf.readframes(self.chunk)
-        while data:
-            if not goal_handle.is_active:
-                return TTS.Result()
-
-            if goal_handle.is_cancel_requested:
-                goal_handle.canceled()
-                return TTS.Result()
-
-            audio_data_msg = data_to_msg(data, audio_format)
-            if audio_data_msg is None:
-                self.get_logger().error(f"Format {audio_format} unknown")
-                self._goal_handle.abort()
-                return TTS.Result()
-
-            msg = AudioStamped()
-            msg.header.frame_id = self.frame_id
-            msg.header.stamp = self.get_clock().now().to_msg()
-            msg.audio.audio_data = audio_data_msg
-            msg.audio.info.format = audio_format
-            msg.audio.info.channels = wf.getnchannels()
-            msg.audio.info.chunk = self.chunk
-            msg.audio.info.rate = wf.getframerate()
-
-            self.player_pub.publish(msg)
-            pub_rate.sleep()
-
+            # send audio data
             data = wf.readframes(self.chunk)
+
+            while data:
+                pub_rate.sleep()
+
+                if not goal_handle.is_active:
+                    return TTS.Result()
+
+                if goal_handle.is_cancel_requested:
+                    goal_handle.canceled()
+                    return TTS.Result()
+
+                audio_data_msg = data_to_msg(data, audio_format)
+                if audio_data_msg is None:
+                    self.get_logger().error(f"Format {audio_format} unknown")
+                    self._goal_handle.abort()
+                    return TTS.Result()
+
+                msg = AudioStamped()
+                msg.header.frame_id = self.frame_id
+                msg.header.stamp = self.get_clock().now().to_msg()
+                msg.audio.audio_data = audio_data_msg
+                msg.audio.info.format = audio_format
+                msg.audio.info.channels = wf.getnchannels()
+                msg.audio.info.chunk = self.chunk
+                msg.audio.info.rate = wf.getframerate()
+
+                self.player_pub.publish(msg)
+
+                data = wf.readframes(self.chunk)
+
+        else:
+            # Stream chunks
+            # These values might be specific to the model?
+            audio_format = pyaudio.paInt16
+            # audio_format = pyaudio.paFloat32
+            rate = 24000
+            channels = 1
+            frequency = rate / self.chunk
+            pub_rate = self.create_rate(frequency)
+
+            self.get_logger().debug(f"Streaming chunks")
+
+            # TODO: TTS/tts/layers/xtts/stream_generator.py:138: UserWarning: You have modified the pretrained model configuration to control generation. This is a deprecated strategy to control generation and will be removed soon, in a future version. Please use a generation configuration file (see https://huggingface.co/docs/transformers/main_classes/text_generation)
+
+            chunks = self.tts.synthesizer.tts_model.inference_stream(
+                text,
+                speaker_wav=self.speaker_wav,
+                speaker=self.speaker,
+                language=language,
+                gpt_cond_latent=self.gpt_cond_latent, 
+                speaker_embedding=self.speaker_embedding,
+                stream_chunk_size=self.chunk,
+                temperature=0.65,
+                repetition_penalty=10.0,
+                speed=1.0,
+                enable_text_splitting=True,
+            )
+            
+            for i, data in enumerate(chunks):
+                self.get_logger().debug(f"Streaming chunk number {i}")
+
+                if not goal_handle.is_active:
+                    return TTS.Result()
+
+                if goal_handle.is_cancel_requested:
+                    goal_handle.canceled()
+                    return TTS.Result()
+                
+                data = data.clone().detach().cpu().numpy()
+                data = data[None, : int(data.shape[0])]
+                data = np.clip(data, -1, 1)
+                data = (data * 32767).astype(np.int16)
+
+                audio_data_msg = data_to_msg(data.tobytes(), audio_format)
+                if audio_data_msg is None:
+                    self.get_logger().error(f"Format {audio_format} unknown")
+                    self._goal_handle.abort()
+                    return TTS.Result()
+
+                msg = AudioStamped()
+                msg.header.frame_id = self.frame_id
+                msg.header.stamp = self.get_clock().now().to_msg()
+                msg.audio.audio_data = audio_data_msg
+                msg.audio.info.format = audio_format
+                msg.audio.info.channels = channels
+                msg.audio.info.chunk = self.chunk
+                msg.audio.info.rate = rate
+
+                self.player_pub.publish(msg)
+                end_time = time.time()
+                self.get_logger().debug(f"Text to speech chunk {i} {end_time - start_time} seconds")
+                pub_rate.sleep()
+
+        end_time = time.time()
+        self.get_logger().debug(f"Text to speech took {end_time - start_time} seconds")
+
 
         goal_handle.succeed()
         return TTS.Result()
